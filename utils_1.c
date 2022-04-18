6dbd5ff7f651bb8b163ed4d2a4c0eb1d7af41747
Nabila nadia Md zaid <nabmd-za@u82z04s02.42kl.edu.my>
2022-04-18 21:01:02 +0800

    Update utils_1

--- utils_1.c
+++ utils_1.c
@@ -6,7 +6,7 @@
 /*   By: nabmd-za <nabmd-za@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 00:34:08 by nabmd-za          #+#    #+#             */
-/*   Updated: 2022/04/13 01:39:44 by nabmd-za         ###   ########.fr       */
+/*   Updated: 2022/04/13 16:47:05 by nabmd-za         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_putchar(char c)
 
 int	ft_putstr(char *s)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	if (s == NULL)
